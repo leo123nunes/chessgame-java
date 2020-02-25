b451af1513f3fddc57e0e8637c2c44bf0aedb0c0
leo123nunes <ti.userleo@hotmail.com>
2020-02-24 21:18:58 -0300

    Setting the class with rows and columns

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Testing");
+		Position position = new Position(1,3);
+		
+		System.out.println(position);
 		
 	}
 
