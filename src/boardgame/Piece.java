10887dec3bee32c0b14c34a989d0654627741fe3
leo123nunes <ti.userleo@hotmail.com>
2020-03-11 11:16:33 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -3,12 +3,10 @@ package boardgame;
 public class Piece {
 	protected Position position;
 	private Board board;
-	
-	public Piece() {
-	}
 
-	public Piece(Position position) {
-		this.position = position;
+	public Piece(Board board) {
+		this.board = board;
+		this.position = null;
 	}
 
 	protected Board getBoard() {
