9ccf9deef5114646bf4b547973b76c07ac6e8676
leo123nunes <ti.userleo@hotmail.com>
2020-03-11 10:29:07 -0300

    Starting to implement board and piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,17 @@
 package boardgame;
 
 public class Piece {
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+	}
 
+	public Piece(Position position) {
+		this.position = position;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
 }
